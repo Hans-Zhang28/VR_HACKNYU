54ca68642a36edf5260a03d5e3285f20598d4c6c
Vincent Vuong <vincent.vuong@compass.com>
2017-02-18 17:58:10 -0500

    spawning works

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -9,36 +9,29 @@ public class FoodSpawner : MonoBehaviour {
 	public int itemsToDrop = 100;
 	public float dropDelay = 1f;
 	public float currentTime = 0f;
-	private bool droppingItems = false;
 
 	// Use this for initialization
 	void Start () {
 		currentTime = dropDelay;
-		droppingItems = true;
-		dropItems ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (droppingItems && currentTime >= 0) {
-			currentTime -= Time.deltaTime;
-		}
-	}
-
-	void dropItems() {
-		while (itemsToDrop > 0) {
-			if (currentTime < 0) {
+		if (itemsToDrop > 0) {
+			if (currentTime >= 0) {
+				currentTime -= Time.deltaTime;
+			} else {
 				makeItem (foods [Random.Range (0, foods.Length)]);
+				Debug.Log ("Making Item! " + itemsToDrop);
 				currentTime = dropDelay;
 				itemsToDrop--;
 			}
 		}
-		droppingItems = false;
 	}
 
 	void makeItem(GameObject item) {
 		Vector3 pos = spawnPoints [Random.Range (0, spawnPoints.Length)].position;
-		GameObject newItem = Instantiate (item, pos, Quaternion.identity);
+		GameObject newItem = Instantiate (item, pos, Quaternion.identity) as GameObject;
 		//newItem.GetComponent<Transform>().position = pos;
 	}
 }
